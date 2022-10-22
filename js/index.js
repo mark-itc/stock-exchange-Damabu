b3f462db9799889e0ddf9ab6585d668dcafb6ef1
Damabu <daniel.mabus@me.com>
2022-10-22 23:10:20 +0300

    Milestone 3

--- js/index.js
+++ js/index.js
@@ -18,8 +18,9 @@ btn.onclick = () => {
 
 
 async function showData (){
-    
+
     if(searchInput.value == ''){
+        console.log("No hacemos nada por que no hay datos que mostrar")
         return ;
     }
 
@@ -38,14 +39,49 @@ async function showData (){
     console.log(data)
 
     let htmldata = data.map((el)=>{
-        return `<li class="list-group-item"><a href="company.html?symbol=${el.symbol}">${el.name} (${el.symbol})</a></li>`;
+        let promise = getCompanyInfo(el.symbol);
+
+        promise.then((data) => {
+            
+            let img = document.getElementById(data.symbol + "_img");
+            img.src = data.profile.image;
+
+            let percent = document.getElementById(data.symbol + "_percent");
+
+            if(data.profile.changesPercentage >= 0){
+                percent.innerHTML = `<span style='color:green;'>+${data.profile.changesPercentage}%</span>`
+            }else{
+                percent.innerHTML = `<span style='color:red;'>${data.profile.changesPercentage}%</span>`
+            }
+
+            
+
+
+        })
+
+        return `<li class="list-group-item">
+            <img id="${el.symbol}_img" style='max-height:30px;' src=''>
+            <a href="company.html?symbol=${el.symbol}">${el.name}</a>
+            <span style='font-size:12px; color:gray;'>(${el.symbol})</span>
+            <div id="${el.symbol}_percent" style='display:inline-block;'></div>
+        </li>`;
     }).join('');
 
     let listahtml = `<ul class="list-group">${htmldata}</ul>`
     result.innerHTML = listahtml
 }
 
+async function getCompanyInfo(symbol){
+    const endpoint = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/";
+     
+    let url = endpoint + symbol;
 
+    let response = await fetch(url);
+    let promise = await response.json();
+
+    return promise;
+
+}
 
 searchInput.addEventListener('keyup', debounce(function(){
     showData()
